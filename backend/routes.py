431e51179378cff43a7765b3b0661ae98caf2221
Artachtron <florent.vaucher@gmail.com>
2023-12-15 19:13:11 -0500

    implemented pictures service

--- backend/routes.py
+++ backend/routes.py
@@ -35,7 +35,7 @@ def count():
 ######################################################################
 @app.route("/picture", methods=["GET"])
 def get_pictures():
-    pass
+    return data
 
 ######################################################################
 # GET A PICTURE
@@ -44,7 +44,10 @@ def get_pictures():
 
 @app.route("/picture/<int:id>", methods=["GET"])
 def get_picture_by_id(id):
-    pass
+    for pic in data:
+        if pic['id'] == id:
+            return pic
+    return abort(404)
 
 
 ######################################################################
@@ -52,7 +55,15 @@ def get_picture_by_id(id):
 ######################################################################
 @app.route("/picture", methods=["POST"])
 def create_picture():
-    pass
+    picture = request.get_json()
+    for pic in data:
+        if picture['id'] == pic['id']:
+            message = {"Message": f"picture with id {picture['id']} already present"}
+            return jsonify(message), 302
+
+    data.append(picture)
+    return picture, 201
+    
 
 ######################################################################
 # UPDATE A PICTURE
@@ -61,11 +72,25 @@ def create_picture():
 
 @app.route("/picture/<int:id>", methods=["PUT"])
 def update_picture(id):
-    pass
+    picture_data = request.get_json()
+    for i, pic in enumerate(data):
+        if pic['id'] == id:
+            data[i] = picture_data
+            return data[i]
+    
+    return {"message": "picture not found"}, 404
+    
+
+    
+
 
 ######################################################################
 # DELETE A PICTURE
 ######################################################################
 @app.route("/picture/<int:id>", methods=["DELETE"])
 def delete_picture(id):
-    pass
+    for pic in data:
+        if pic['id'] == id:
+            data.remove(pic)
+            return data, 204
+    return {"message": "picture not found"}, 404
